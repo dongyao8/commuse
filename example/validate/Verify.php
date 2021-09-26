6ebea2f5ef5fb068ef21ee242f0893f26f713da2
clark <243512842@qq.com>
2021-09-26 17:27:56 +0800

    init

--- example/validate/Verify.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../../vendor/autoload.php');
-
-use Dongyao8\Commuse\validate\Verify;
-
-$name = 'test123';
-$verify = new Verify;
-$checkres = $verify::checkHasChinese($name);
-var_dump($checkres);
