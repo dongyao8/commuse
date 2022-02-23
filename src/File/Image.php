12465ec02fc89be86deb47135c85f8945be2478b
clark <243512842@qq.com>
2022-02-23 11:50:46 +0800

    ::bookmark:: V1.0.0

--- src/File/Image.php
+++ src/File/Image.php
@@ -74,5 +74,39 @@ class Image{
 
 
 
+    /**
+     * Base64生成图片文件,自动解析格式
+     * @param $base64
+     * @param $path
+     * @param $filename
+     * @return array
+     */
+    public static function base64ToImage($base64, $path, $filename)
+    {
+        $res = array();
+        //匹配base64字符串格式
+        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64, $result)) {
+            //保存最终的图片格式
+            $postfix = $result[2];
+            $base64 = base64_decode(substr(strstr($base64, ','), 1));
+            $filename .= '.' . $postfix;
+            $path .= $filename;
+            //创建图片
+            if (file_put_contents($path, $base64)) {
+                $res['status'] = 1;
+                $res['filename'] = $filename;
+            } else {
+                $res['status'] = 2;
+                $res['err'] = 'Create img failed!';
+            }
+        } else {
+            $res['status'] = 2;
+            $res['err'] = 'Not base64 char!';
+        }
+        return $res;
+    }
+
+
+
 
 }
