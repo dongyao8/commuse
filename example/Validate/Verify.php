22f742b240e2d1bf069c7ef92b827c8fc9f5360d
clark <243512842@qq.com>
2023-02-20 14:05:42 +0800

    扩充电信号段

--- example/Validate/Verify.php
+++ example/Validate/Verify.php
@@ -3,7 +3,12 @@ require_once('../../vendor/autoload.php');
 
 use Dongyao8\Commuse\validate\Verify;
 
-$name = 'test123';
+// $name = 'test123';
+// $verify = new Verify;
+// $checkres = $verify::checkHasChinese($name);
+// var_dump($checkres);
+
+$name = '19243714913';
 $verify = new Verify;
-$checkres = $verify::checkHasChinese($name);
+$checkres = $verify::checkMobile($name);
 var_dump($checkres);
