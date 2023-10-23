d96aedcb5f03e1972288fa187402ab2f82143de0
clark <243512842@qq.com>
2023-10-23 12:39:23 +0800

    fix:更改手机号验证规则

--- example/Validate/Verify.php
+++ example/Validate/Verify.php
@@ -3,7 +3,7 @@ require_once('../../vendor/autoload.php');
 
 use Dongyao8\Commuse\validate\Verify;
 
-$name = 'test123';
+$mobile = '16702030405';
 $verify = new Verify;
-$checkres = $verify::checkHasChinese($name);
+$checkres = $verify::checkMobile($mobile);
 var_dump($checkres);
--- src/Validate/Verify.php
+++ src/Validate/Verify.php
@@ -55,17 +55,8 @@ class Verify
      */
     public static function checkMobile($phone_number)
     {
-        //中国联通号码：130、131、132、145（无线上网卡）、155、156、185（iPhone5上市后开放）、186、176（4G号段）、175（2015年9月10日正式启用，暂只对北京、上海和广东投放办理）,166,146
-        //中国移动号码：134、135、136、137、138、139、147（无线上网卡）、148、150、151、152、157、158、159、178、182、183、184、187、188、198
-        //中国电信号码：133、153、180、181、189、177、173、149，191，193，199
-        $g = "/^1[34578]\d{9}$/";
-        $g2 = "/^19[13589]\d{8}$/";
-        $g3 = "/^166\d{8}$/";
+        $g = "/^1[3456789]\d{9}$/";
         if (preg_match($g, $phone_number)) {
-            return true;
-        } else  if (preg_match($g2, $phone_number)) {
-            return true;
-        } else if (preg_match($g3, $phone_number)) {
             return true;
         }
         return false;
